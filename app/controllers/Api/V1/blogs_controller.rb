2aa9c6d8c397e7c6f703d30dac885ed48887e19f
Lawrence D <juxlarry@gmail.com>
2023-06-14 11:20:19 +0000

    Added jwt authentication check to endpoints

--- app/controllers/Api/V1/blogs_controller.rb
+++ app/controllers/Api/V1/blogs_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::BlogsController < ApplicationController
+    before_action :authenticate_user!
     before_action :set_blog, only: %i[show update destroy]
 
     #Get /Blogs
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_13_232840) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_14_111547) do
   create_table "blogs", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
-    t.string "content"
+    t.text "content"
     t.string "author"
     t.bigint "category_id", null: false
     t.datetime "created_at", null: false
