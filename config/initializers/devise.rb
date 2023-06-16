13c80520d17007a7293387ebd6c9bff280fa56d3
Lawrence D <juxlarry@gmail.com>
2023-06-16 12:49:42 +0000

    Secrets yml

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -311,6 +311,8 @@ Devise.setup do |config|
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
 
+  config.secret_key = ENV["SECRET_KEY_BASE"] if Rails.env.production?
+
   config.jwt do |jwt|
     jwt.secret = Rails.application.credentials.fetch(:secret_key_base)
     
